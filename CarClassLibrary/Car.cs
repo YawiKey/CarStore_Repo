865779caa6980bacbc555e97fd52eb00109b90c8
Yawi <ana.garciamondejar@gmail.com>
2024-11-03 20:26:54 +0100

    Add project files.

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace CarClassLibrary
+{
+    public class Car
+    {
+        //Propierties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        // Default Constructor
+        public Car()
+        {
+            this.Make = " - ";
+            this.Model = " - ";
+            this.Price = 0.00M;
+
+
+        }
+
+        //Parametrized Constructor
+
+        public Car(string a, string b, decimal c)
+        {
+
+            this.Make = a;
+            this.Model = b;
+            this.Price = c;
+        }
+
+    }
+}
---
+++ CarShopConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CarClassLibrary;
+
+
+Console.WriteLine("Hello, World!");
+
+
+Car car = new Car();
