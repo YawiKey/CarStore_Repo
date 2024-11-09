b8ea86756472226563b9c637c69ceba3f129a014
Yawi <yawikraft@gmail.com>
2024-11-09 18:51:30 +0100

    ObjectInstance_Car
    Added two diferent ways to use the constructor in the library Class

--- CarShopConsoleApp/Program.cs
+++ CarShopConsoleApp/Program.cs
@@ -5,4 +5,16 @@ using CarClassLibrary;
 Console.WriteLine("Hello, World!");
 
 
-Car car = new Car();
+//01. GENERIC INSTANCE
+Car car01 = new Car();
+
+// Tha call is made by referencing the instante object and then the function
+Console.WriteLine("Car stored is" + car01.Make + car01.Model + car01.Price);
+
+//02. PARAMETRIZED INSTANCE
+//The instance of the object is pre-made with the parameters
+Car car02 = new Car(" Artemis ", " Peugeot ", 1200);
+//The parameters substitute the standard in the constructor
+Console.WriteLine("Car stored is" + car02.Make + car02.Model + car02.Price);
+
+Console.ReadLine();
